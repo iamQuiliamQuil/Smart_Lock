46c9295d1f1b62c2ac2981f83696dc3682ca740e
jhony <jalvarezdelgado@albany.edu>
2022-03-07 18:45:29 -0500

    - Implemented app icon for Android version. ("application_logo.png") - Added "application_logo.png" to iOS resources, implementation missing

--- SmartLock Demo.Android/MainActivity.cs
+++ SmartLock Demo.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace SmartLock_Demo.Droid
 {
-    [Activity(Label = "SmartLock_Demo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SmartLock", Icon = "@drawable/application_logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
