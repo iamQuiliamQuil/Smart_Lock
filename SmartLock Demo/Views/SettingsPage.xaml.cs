d81f2f37d22a4010602991e502f786a9877aa401
jhony <jalvarezdelgado@albany.edu>
2022-05-09 19:37:11 -0400

    - Added comments to some functions in SettingsPage.xaml.cs and UserPage.xaml.cs for documentation

--- SmartLock Demo/Views/SettingsPage.xaml.cs
+++ SmartLock Demo/Views/SettingsPage.xaml.cs
@@ -16,9 +16,7 @@ namespace SmartLock_Demo.Views
         public SettingsPage()
         {
             InitializeComponent();
-            PhoneRegister.IsEnabled = false;
-            registerCell = PhoneRegister;
-            switch (Preferences.Get("wantsText", false))
+            switch (Preferences.Get("wantsText", false)) //Used to keep the Switchcell state between sessions
             {
                 case true:
                     TextBool.On = true;
@@ -28,26 +26,27 @@ namespace SmartLock_Demo.Views
                     break;
             }
         }
+        
+        //TextSwitch toggles the wantsText boolean kept in Preferences, called when the Switchcell TextBool is toggled
         private async void TextSwitch(object sender, EventArgs e)
         {
-            bool wantsText = TextBool.On;
-            switch (wantsText)
+            bool wantsText = TextBool.On; //Checks if the switch is on or off, sets wantsText to the result
+            switch (wantsText) //Switch statement based on whether the switch is on or off
             {
                 case true:
                     Preferences.Set("wantsText", true);
-                    PhoneRegister.IsEnabled = true;
+                    PhoneRegister.IsEnabled = true; //Allows the Entrycell to receive input
                     PhoneRegister.Placeholder = "Your Phone Number Here";
-                    if(Preferences.Get("phoneNumber", "").Equals(""))
+                    if(Preferences.Get("phoneNumber", "").Equals("")) //Checks if the phone number hasn't been set to anything, if so it reminds the user to set their phone number
                     {
                         await DisplayAlert("SMS enabled", "SMS notifications have been enabled. Please update your phone number", "OK");
 
                     }
-                    //TableSection.Add(registerCell);
                     break;
                 default:
                     Preferences.Set("wantsText", false);
-                    Preferences.Remove("phoneNumber");
-                    PhoneRegister.IsEnabled = false;
+                    Preferences.Remove("phoneNumber"); //Clears phone number from the app preferences, we need to be ethical
+                    PhoneRegister.IsEnabled = false; //Makes it so the Entrycell can't receive input
                     PhoneRegister.Placeholder = "SMS disabled";
                     PhoneRegister.Text = "";
                     await DisplayAlert("SMS disabled", "Understood, SMS notifications disabled and phone number has been cleared from application preferences", "OK");
@@ -56,14 +55,16 @@ namespace SmartLock_Demo.Views
             }
             //await DisplayAlert("Switch changed", "State currently " + wantsText, "OK"); Debugging
         }
+
+        //UpdatePhone is used to verify the format of the phone number input, if valid it updates the phoneNumber preference
         private async void UpdatePhone(object sender, EventArgs e)
         {
             string phoneNumber = PhoneRegister.Text;
-            bool valid = true;
+            bool valid = true; //Assume it's true. If one of the conditions is fulfilled, then it isn't a true phone number
             //Verify that this is a phone number (all numbers)
             for(int i = 0; i < phoneNumber.Length; i++)
             {
-                if (phoneNumber[i] < 48 || phoneNumber[i] > 57)
+                if (phoneNumber[i] < 48 || phoneNumber[i] > 57) //Checks ASCII values, only digits
                 {
                     valid = false;
                 }
@@ -79,14 +80,17 @@ namespace SmartLock_Demo.Views
                 await DisplayAlert("Phone Number Set!", "Phone number successfully set to: " + phoneNumber, "OK");
             }
         }
+
+        //SetIP is used to set the IP address in the application preferences, called when the IPADDR Entrycell receives input
         private async void SetIP(object sender, EventArgs e)
         {
             string ipAddress = IPADDR.Text;
-            if(ipAddress == null)
+            if(ipAddress == null) //If you were to open the keyboard and not type anything, the app would crash if not for this
             {
                 return;
             }
 
+            //Dot and colon used to separate IP Address and Port
             char ch = '.';
             char col = ':';
             int dots = ipAddress.Count(f => (f == ch)); //Counts how many periods in input
